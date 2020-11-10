08bf4cf90de7baf3496e4152332630139d03b8c5
amit9503 <gosaviamit07@gmail.com>
2020-11-10 10:43:47 +0530

    New test cases for automtion testing of APIS

---
+++ consuming_apis/sample_requests.py
@@ -0,0 +1,40 @@
+import requests
+
+# BASE_URL = "http://api.weatherstack.com"
+#
+# payload = params = {"access_key": "bb602de71853fee2d5218640352583ee", "query":"pune"}
+#
+# result = requests.get(f"{BASE_URL}/current", params=payload)
+# print(result.text)
+
+
+
+
+# BASE_URL = "https://newsapi.org/"
+#
+# params = {"country": "us"}
+# headers= {"Authorization": "Bearer da49e89b21b24d12a19a9fd974afc103"}
+#
+# result = requests.get(f"http://craphound.com/images/1006884_2adf8fc7.jpg")
+
+# print(dir(result))
+
+# with open("sample.png", 'wb') as f:
+#     f.write(result.content)
+
+BASE_URL = "http://httpbin.org/"
+
+class Temprature:
+
+    temp =
+
+result['current']['temprature']
+
+Temprature(temp=result['current']['temprature'],humidity = )
+
+
+
+data = {"name": "bitybybit", "day":"wednesday"}
+
+result = requests.post(f"{BASE_URL}/post", data=data)
+print(result.text)
